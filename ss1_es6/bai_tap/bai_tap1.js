0777d26ed5a43f4c3a13c6af17b5406c20e9337b
TranDinhNgocSang <sangdolphin98@gmail.com>
2023-07-17 13:36:04 +0700

    17/07/2023

---
+++ ss1_es6/bai_tap/bai_tap1.js
@@ -0,0 +1,63 @@
+let courses = [
+    {
+        id: 1,
+        title: "ReactJS Tutorial",
+        rating: 4.2,
+    },
+    {
+        id: 2,
+        title: "Angular Tutorial",
+        rating: 2.5,
+    },
+    {
+        id: 3,
+        title: "VueJS Tutorial",
+        rating: 3.8,
+    },
+    {
+        id: 4,
+        title: "Java Tutorial",
+        rating: 4,
+    },
+    {
+        id: 5,
+        title: "JavaScript Tutorial",
+        rating: 3.5,
+    },
+];
+
+// 1.  Sử dụng cú pháp ES6 để xuất ra màn hình danh sách các bài đăng có rating ≥ 4
+let ratingMore4 = courses.filter((obj) => {
+    return obj.rating >= 4
+})
+console.log(ratingMore4);
+
+// 2.Sử dụng cú pháp ES6 để xuất ra màn hình danh sách các bài đăng có rating < 4 với yêu cầu giá trị các phần tử của mảng mới có định dạng: 
+let ratingLess4 = courses.filter((rating) => {
+    return rating.rating < 4
+}).map((courses) => {
+    return courses.id + "-" + courses.title + "-" + courses.rating
+})
+console.log(ratingLess4);
+
+
+let addedCourses = [
+    {
+        id: 6,
+        title: "PHP Tutorial",
+        rating: 3,
+    },
+    {
+        id: 7,
+        title: "C# Tutorial",
+        rating: 2,
+    },
+    {
+        id: 8,
+        title: "Docker Tutorial",
+        rating: 3.8,
+    }
+];
+// 3.Sử dụng cú pháp ES6 đã học viết hàm trả về một mảng mới gộp 2 mảng courses và addedCourses
+let mergeArr = [...courses, ...addedCourses];
+console.log(mergeArr);
